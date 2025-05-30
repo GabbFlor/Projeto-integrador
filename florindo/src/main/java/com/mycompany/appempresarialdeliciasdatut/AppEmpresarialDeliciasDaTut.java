ab89b391b0ee329117d7898d7b41299f13d459b1
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2025-05-30 18:24:02 -0300

    feat
    projeto pronto (esqueci de upar antes)

---
+++ florindo/src/main/java/com/mycompany/appempresarialdeliciasdatut/AppEmpresarialDeliciasDaTut.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.appempresarialdeliciasdatut;
+
+/**
+ *
+ * @author flor
+ */
+public class AppEmpresarialDeliciasDaTut {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
